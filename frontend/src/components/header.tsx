7f29661675bb9c2e047c3602317783b2b511d9ed
AbdunazarovHikmatullo <abdunazarovhikmatullo2@gmail.com>
2025-08-10 23:27:21 +0500

    header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,9 +1,9 @@
-
+import { ModeToggle } from "./mode-toggle"
 
 export default function Header(){
     return(
         <header className="border-b mx-auto max-w-screen-xl py-5 px-2">
-            
+            <ModeToggle/>
         </header>
     )
 }

d9c89b6b0f2191082370cf64be2d9597632d4c02
MrEx3cut0r <legendary.killtell@gmail.com>
2025-08-18 22:38:06 +0300

    docker compose added

--- backend/product_service/main.py
+++ backend/product_service/main.py
@@ -18,4 +18,4 @@ def app() -> FastAPI:
     return app
 
 if __name__=="__main__":
-    uvicorn.run("main:app", host="localhost", port=8002)
+    uvicorn.run("main:app", host="localhost", port=8000)

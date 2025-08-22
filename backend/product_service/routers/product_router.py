b944b8f99087ba53e987af07f4e2a77e185c1276
MrEx3cut0r <legendary.killtell@gmail.com>
2025-08-22 21:47:20 +0300

    search improved

--- backend/product_service/routers/product_router.py
+++ backend/product_service/routers/product_router.py
@@ -1,16 +1,22 @@
 from fastapi import APIRouter, Depends
 from daos.product_dao import getProductDAO
+from models.product_model import ProductModel
+from typing import Optional, List
 
 router = APIRouter(prefix="/products")
 
 @router.get("/")
-def getAll(service: getProductDAO = Depends()):
+def getAll(service: getProductDAO = Depends()) -> List[ProductModel]:
 	return service.getAll()
 
 @router.get("/search")
-def getById(id: int, service: getProductDAO = Depends()):
-	return service.getById(id)
+def search(
+		id: int = 0, 
+		username: str = "", 
+		service: getProductDAO = Depends()
+	) -> Optional[ProductModel] | List:
 
-@router.delete("/delete")
-def deleteByid(id:int, service: getProductDAO = Depends()):
-	return service.delete(id)
+	product = service.getById(id)
+	if username != "":
+		product = service.searchByUsername(username)
+	return product

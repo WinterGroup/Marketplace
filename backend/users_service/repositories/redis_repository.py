75e4058baf560617b48b00b77dc0982dc3db1942
MrEx3cut0r <legendary.killtell@gmail.com>
2025-08-24 16:46:31 +0300

    users_service remaked to asynchronius

--- backend/users_service/repositories/redis_repository.py
+++ backend/users_service/repositories/redis_repository.py
@@ -1,26 +1,26 @@
 from pydantic import BaseModel
 from typing import Optional
 import pickle
-import redis
+from redis.asyncio import Redis
 
 class RedisRepository():
 	def __init__(self):
-		self.r = redis.Redis(
+		self.r = Redis(
 			host="redis",
 			port=6379,
 		)
 	
-	def setItem(self, key: Optional[str] | int, item: BaseModel) -> BaseModel:
+	async def setItem(self, key: Optional[str] | int, item: BaseModel) -> BaseModel:
 		pickled = pickle.dumps(item)
-		self.r.set(key, pickled)
+		await self.r.set(key, pickled)
 		return item
 
-	def getItem(self, key: Optional[str] | int) -> Optional[BaseModel] | None:
-		item = self.r.get(key)
+	async def getItem(self, key: Optional[str] | int) -> Optional[BaseModel] | None:
+		item = await self.r.get(key)
 		return pickle.loads(item) if item else None
 	
-	def delete(self, key: Optional[str] | int):
-		self.r.delete(key)
+	async def delete(self, key: Optional[str] | int):
+		await self.r.delete(key)
 
 
 def getRedisRepository() -> RedisRepository:
